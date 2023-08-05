629ea80d210485f2815068ffb12362a45b2a017e
JakeEh <alsemgeester@gmail.com>
2023-08-05 10:31:59 -0400

    Control your computer with Python!

--- ComputerControl/control.py
+++ ComputerControl/control.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pyautogui
+import keyboard
 import time
 
 
@@ -8,50 +9,12 @@ pyautogui.moveTo(100, 500, duration=1)
 
 time.sleep(2) # 2 seconds
 
-pyautogui.moveTo(500, None, duration=1.5) # uses the same Y as before
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pyautogui.moveTo(500, 500, duration=1.5) # uses the same Y as before
 
 pyautogui.click()
 
 # You can also click at specific coordinates
 # pyautogui.click(x, y)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 pyautogui.typewrite("Hello, world!", interval=0.2)
 pyautogui.press('enter')
@@ -59,74 +22,21 @@ pyautogui.keyDown('shift')
 pyautogui.write('uppercase')
 pyautogui.keyUp('shift')
 
+print(pyautogui.alert(text="text", title="title", button="OK"))
 
+print(pyautogui.confirm(text='text', title='title', buttons=['OK', 'CANCEL'])) # returns text of button clicked on
 
+print(pyautogui.prompt(text='text', title='title', default='default')) # returns input text
 
+print(pyautogui.password(text='text', title='title', default='default', mask='*'))
 
+counter = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(pyautogui.alert(text="text", title="title", button="OK"))
-
-# print(pyautogui.confirm(text='text', title='title', buttons=['OK', 'CANCEL'])) # returns text of button clicked on
-
-# print(pyautogui.prompt(text='text', title='title', default='default')) # returns input text
-
-# print(pyautogui.password(text='text', title='title', default='default', mask='*'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import keyboard
-
-# counter = 0
-
-# def saveScreenshot():
-#     global counter
-#     filename = "screenshot-" + str(counter) + ".png"
-#     pyautogui.screenshot(filename)
-#     counter = counter + 1
+def saveScreenshot():
+    global counter
+    filename = "screenshot-" + str(counter) + ".png"
+    pyautogui.screenshot(filename)
+    counter = counter + 1
     
-# keyboard.add_hotkey('space', saveScreenshot)
-# keyboard.wait()
-
+keyboard.add_hotkey('space', saveScreenshot)
+keyboard.wait()
